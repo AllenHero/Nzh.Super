6a6bc7564016e5b75f830b0179a2b30c43636d9b
倪泽恒 <644508159@qq.com>
2020-04-15 17:47:50 +0800

    添加底层。

---
+++ Nzh.Super.Extension/DapperExtAllSQL.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Nzh.Super.Extension
+{
+    public static class DapperExtAllSQL
+    {
+        public static DataTable GetDataTableBase(this IDbConnection conn, string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null)
+        {
+            using (IDataReader reader = conn.ExecuteReader(sql, param, transaction, commandTimeout))
+            {
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                return dt;
+            }
+        }
+
+        public static DataSet GetDataSetBase(this IDbConnection conn, string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null)
+        {
+            using (IDataReader reader = conn.ExecuteReader(sql, param, transaction, commandTimeout))
+            {
+                DataSet ds = new DataSet();
+                while (!reader.IsClosed)
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    ds.Tables.Add(dt);
+                }
+                return ds;
+            }
+        }
+    }
+}
---
+++ Nzh.Super.Extension/DapperExtAttributes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nzh.Super.Extension
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TableAttribute : Attribute
+    {
+        public string Name { get; set; }
+        public TableAttribute(string tableName)
+        {
+            this.Name = tableName;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class KeyAttribute : Attribute
+    {
+        public bool IsIdentity { get; set; }
+        public KeyAttribute(bool isidentity)
+        {
+            IsIdentity = isidentity;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ComputedAttribute : Attribute
+    {
+
+    }
+}
